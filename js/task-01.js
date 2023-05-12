3a2280c6d5b0302f821657db3fd54d9d94123090
Bartosz Drzazga <bartosz.drzazga30@gmail.com>
2023-05-12 16:19:15 +0200

    git innit

--- js/task-01.js
+++ js/task-01.js
@@ -19,6 +19,7 @@ console.log(`Number of categories: ${categoryCounter}`);
 
 const categoryNames = categoryItems.forEach((item) => {
   const header = item.querySelector("h2").textContent;
+  // const elementsCounter = item.querySelector("ul").childElementCount;
   const elementsCounter = item.querySelectorAll("li").length;
   console.log("\n");
   console.log(`Category: ${header}`);
--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,27 @@
+// Napisz skrypt, który dla każdego elementu tablicy ingredients:
+
+// 1. Utworzy oddzielny element <li>. Obowiązkowo użyj metody document.createElement().
+// 2. Doda nazwę elementu (string z tablicy) jako jego zawartość tekstową.
+// 3. Doda do elementu klasę item.
+// 4. Następnie jedna operacja umieści wszystkie <li> na liście ul#ingredients.
+
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsList = document.querySelector("#ingredients");
+let liContainer = [];
+
+ingredients.forEach((ingredient) => {
+  const liCreator = document.createElement("li");
+  liCreator.classList.add("item");
+  liCreator.textContent = ingredient;
+  liContainer.push(liCreator);
+});
+ingredientsList.append(...liContainer);
+console.log(liContainer);
